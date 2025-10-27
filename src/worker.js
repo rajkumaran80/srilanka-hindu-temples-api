ac48f728fbf18381d90f317c0786c18b208c071b
Raj Kumaran <raj.kumaran@farthestgate.co.uk>
2025-10-27 12:56:15 +0000

    initial commit

--- src/worker.js
+++ src/worker.js
@@ -1,21 +1,17 @@
-import { MongoClient } from "mongodb";
-
-const uri = "mongodb+srv://srilanka_temples_admin:srilanka_temples_admin123@srilanka-cluster.6k82w97.mongodb.net/hindu-temples?retryWrites=true&w=majority";
-
-let client;
-
 export default {
   async fetch(request, env, ctx) {
-    if (!client) {
-      client = new MongoClient(uri);
-      await client.connect();
+    try {
+      const res = await fetch(env.BACKEND_URL + "/temples");
+      const temples = await res.json();
+
+      return new Response(JSON.stringify(temples), {
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (err) {
+      return new Response(JSON.stringify({ error: err.message }), {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      });
     }
-
-    const db = client.db("hindu-temples");
-    const temples = await db.collection("temples").find().toArray();
-
-    return new Response(JSON.stringify(temples), {
-      headers: { "Content-Type": "application/json" },
-    });
   },
 };
